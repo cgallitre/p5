5b584b64ce45387dad30e7693ad1fa706c82cb1d
Cyril <cyril.gallitre@free.fr>
2020-04-20 17:09:32 +0200

    Gestion formation et design

--- src/Controller/TrainingController.php
+++ src/Controller/TrainingController.php
@@ -10,15 +10,47 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class TrainingController extends AbstractController
 {
     /**
+     * Show all trainings
+     * 
      * @Route("/formations", name="training_index")
      */
     public function index(TrainingRepository $repo)
     {
         // $repo = $this->getDoctrine()->getRepository(Training::class);
+   
         $trainings = $repo->findAll();
+     
 
         return $this->render('training/index.html.twig', [
             'trainings' => $trainings
         ]);
     }
+    
+    /**
+     * Create a training
+     *
+     * @Route("/formations/ajout", name="training_create")
+     * 
+     * @return Response
+     */
+    public function create()
+    {
+        return $this->render('training/new.html.twig');
+    }
+
+    /**
+     * Show one training
+     *
+     * @Route("/formations/{slug}", name="training_show")
+     * @return Response
+     */
+    public function show($slug, Training $training)
+    {
+        // $training = $repo->findOneBySlug($slug);
+
+        return $this->render('training/show.html.twig', [
+            'training' => $training
+        ]);
+    }
+
 }
