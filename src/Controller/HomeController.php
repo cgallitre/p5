a6321bdf65dbb019970c6610cd543c52643dc9a1
Cyril <cyril.gallitre@free.fr>
2020-05-14 16:38:24 +0200

    Formulaire contact

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
+use Symfony\Component\Mime\Email;
 use App\Repository\PortfolioRepository;
 use App\Repository\TestimonialRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -46,4 +50,37 @@ class HomeController extends AbstractController {
         return $this->render('apropos.html.twig');
     }
 
+    /**
+     * Formulaire de contact
+     * @Route("/contact", name="contact_index")
+     * 
+     * 
+     */
+    public function contact(Request $request, MailerInterface $mailer)
+    {
+        $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $contact = $form->getData();
+        
+            $email = (new Email())
+            ->from($contact['email'])
+            ->to('contact@gallitre.fr')
+            ->subject('Contact de ' . $contact['name'])
+            ->html($contact['message']);
+    
+            $mailer->send($email);
+
+            $this->addFlash('success',"Votre message a bien été envoyé. Merci de votre participation.");
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('contact.html.twig', [
+            'form' => $form->createView()
+        ]);
+        
+    }
 }
